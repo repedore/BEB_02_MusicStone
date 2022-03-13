ba93817c3238f221fd520e3c91036850a5336f6d
PCY71 <skypcy71@gmail.com>
2022-03-13 18:24:59 +0900

    redux 개발환경 구축

---
+++ client/src/actions/index.js
@@ -0,0 +1,8 @@
+export const TEST_ADD = "TEST_ADD";
+
+// action functions
+export const testAdd = () => {
+    return {
+        type: TEST_ADD
+    }
+}
--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,4 +1,24 @@
+import { testAdd } from '../actions'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react';
+
 function Main() {
-  return <div>main page</div>;
+  const state = useSelector((state) => state.testReducer);
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(0);
+
+  const handleTestBtn = () => {
+    dispatch(testAdd());
+    setCount(state.count);
+  }
+  return <div>main page
+    <div>
+      <button onClick={() => handleTestBtn()}>
+        테스트 버튼 count 1증가
+      </button>
+      {` ${count}`}
+    </div>
+
+  </div>;
 }
 export default Main;
