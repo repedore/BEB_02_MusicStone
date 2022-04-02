bf870134cd2d90b80ac5b72b7f08a4e4c84d216c
고유진 <zeke@goyujins-MacBook-Pro.local>
2022-04-02 10:06:55 +0900

    :memo: Update Stone Server

--- server/services/stones.services.js
+++ server/services/stones.services.js
@@ -3,8 +3,8 @@ const {
   UserModel,
   TradeModel,
   AlbumModel,
+  MusicianModel,
 } = require("../models/index");
-const { MusicianModel } = require("../models/musician.model");
 const ServiceContract = require("../contracts/ServiceContract");
 
 // AlbumList({id, name})
@@ -29,16 +29,55 @@ const getMyStoneList = async (userId) => {
     // userId로 userAccount가져와서 가진 stone조회
     const user = await UserModel.findOne(
       { id: userId },
-      { account: 1, _id: 0 }
+      { account: 1, musician_id: 1, _id: 0 }
     );
+
+    const isMusician = user.musician_id === 0 ? false : true;
     if (user) {
       const userAccount = user.account;
       // Contract에서 userAccount가 가진 SFTList 가져오기
       const myStoneList = await ServiceContract.getMySFTs(userAccount);
-      const list = myStoneList.map((el) => {
-        return el[0], el[1];
+      const myStoneInfo = myStoneList.map((el) => {
+        return { token_id: el[0], userBalance: el[1] };
       });
-      return myStoneList;
+      let stoneIdList = [];
+      for (let stone of myStoneInfo) {
+        stoneIdList.push(
+          (
+            await StoneModel.findOne(
+              { token_id: stone.token_id },
+              { id: 1, _id: 0 }
+            )
+          ).id
+        );
+      }
+      const info = await StoneModel.aggregate([
+        {
+          $lookup: {
+            from: "musicians",
+            let: {
+              musician_id: "$musician_id",
+              name_korea: "$name_k",
+              name_english: "$name_e",
+              originalname: "$originalname",
+            },
+            pipeline: [
+              {
+                $match: {
+                  $expr: {
+                    $and: [{ $eq: ["$id", "$$musician_id"] }],
+                  },
+                },
+              },
+            ],
+            as: "musicianInfo",
+          },
+        },
+      ]);
+      const stoneInfo = info.filter((el) => {
+        return stoneIdList.includes(el.id);
+      });
+      return { stoneInfo, myStoneInfo, isMusician };
     } else {
       return "";
     }
@@ -49,7 +88,8 @@ const getMyStoneList = async (userId) => {
 
 // insertTradeStone
 const insertTrade = async (sellStoneInfo, userId) => {
-  const { stoneId, seller, quantity, unitPrice, itemId } = sellStoneInfo;
+  const { stoneId, seller, quantity, unitPrice, itemId, tradeId } =
+    sellStoneInfo;
   try {
     const Trade = new TradeModel({
       stone_id: stoneId,
@@ -57,6 +97,7 @@ const insertTrade = async (sellStoneInfo, userId) => {
       price: unitPrice,
       amount: quantity,
       item_id: itemId,
+      trade_id: tradeId,
     });
     return await Trade.save();
   } catch (e) {
@@ -76,6 +117,7 @@ const insertStone = async (stoneInfo, fileInfo, account) => {
       lyrics,
       category,
       totalBalance,
+      tokenId,
     } = stoneInfo;
     const { filename, originalname, path } = fileInfo;
     // account로 UserModel에서 musician_id찾기
@@ -97,6 +139,7 @@ const insertStone = async (stoneInfo, fileInfo, account) => {
       originalname,
       path,
       totalBalance,
+      tokenId,
     });
     return await Stone.save();
   } catch (e) {
@@ -136,7 +179,7 @@ const getSellStone = async (userId, listInfo) => {
 const getStoneDetail = async (stoneId) => {
   try {
     const stoneDetail = await StoneModel.findOne({ id: stoneId });
-    const stoneTrade = await TradeModel.find(
+    const sellList = await TradeModel.find(
       { stone_id: stoneId },
       {
         id: 1,
@@ -145,6 +188,8 @@ const getStoneDetail = async (stoneId) => {
         price: 1,
         musician_id: 1,
         item_id: 1,
+        trade_id: 1,
+        _id: 0,
       }
     );
     const musician = await MusicianModel.findOne(
@@ -155,14 +200,14 @@ const getStoneDetail = async (stoneId) => {
     );
     const minPrice = Math.min.apply(
       null,
-      stoneTrade
+      sellList
         .filter((el) => {
           return el.price !== undefined;
         })
         .map((el) => Number(el.price))
     );
 
-    return { stoneDetail, musician, stoneTrade, minPrice };
+    return { stoneDetail, musician, sellList, minPrice };
   } catch (e) {
     throw Error(e);
   }
@@ -173,17 +218,17 @@ const updateBuyStoneInfo = async (tradeInfo, stoneId) => {
   try {
     const { buyer, seller, quantity, unitPrice, sellAmount, tradeId } =
       tradeInfo;
-    if (quantity === 0) {
-      return "구매수량은 1개 이상이어야 합니다.";
-    } else if (quantity > sellAmount) {
-      return "판매수량보다 더 많은 수량은 구매하실 수 없습니다.";
-    } else {
-      const isOk = await TradeModel.updateOne(
-        { id: tradeId },
-        { $set: { amount: sellAmount - quantity } }
-      );
-      return isOk.acknowledged;
-    }
+    // if (quantity === 0) {
+    //   return "구매수량은 1개 이상이어야 합니다.";
+    // } else if (quantity > sellAmount) {
+    //   return "판매수량보다 더 많은 수량은 구매하실 수 없습니다.";
+    // } else {
+    //   const isOk = await TradeModel.updateOne(
+    //     { id: tradeId },
+    //     { $set: { amount: sellAmount - quantity } }
+    //   );
+    //   return isOk.acknowledged;
+    // }
   } catch (e) {
     throw Error(e);
   }
